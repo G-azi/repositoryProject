5ad7f6550cadbfe4d55e7ab61aa951a3ca6bb645
Gazi Hossan <jahid_reco@yahoo.com>
2015-05-05 21:51:27 +0100

    some more functionality from jannat

--- maths.c
+++ maths.c
@@ -6,5 +6,9 @@ int a=20;
 int b=10;
 int result;
 printf("addition of a and b= %d\n", result=a+b);
-
+printf("substraction of a and b= %d\n", result=a-b);
+printf("multiply of a and b=%d\n", result=a*b);
+printf("devide of a and b=%d\n", result=a/b);
+printf("modulas of a and b = %d",result=a%b);
+printf("equition of a and b = %d",result=a+b+b);
 }
